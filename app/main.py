8bf5e21f9f07da1a45a788ee8ca5b3efde6c731f
Jairo Leyton <jali1993@hotmail.com>
2023-01-25 21:07:23 -0500

    Add .env example and gitignore

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
+import os
 from outlook_email import OutlookEmail
+from dotenv import load_dotenv
 
 
 def run():
+    load_dotenv()
     """This should call the email service depending on the config set"""
-    email_service = OutlookEmail('test@hotmail.com', 'random-password')
+    email_service = OutlookEmail(
+        os.getenv('OUTLOOK_USER', ''), os.getenv('OUTLOOK_TOKEN', '')
+    )
     email_service.login()
     emails = email_service.get_unseen_emails()
 
