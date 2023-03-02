b37e0bc5760dc90a4f081443088dddfa44690479
Jairo Leyton <jali1993@hotmail.com>
2023-03-01 21:32:09 -0500

    Remove cleaning functions from the outlook class

--- app/outlook_email.py
+++ app/outlook_email.py
@@ -1,13 +1,10 @@
 from dataclasses import dataclass
 from app.exceptions import (
     UnableRetrieveEmailsException, UnableRetrieveSubjectException,
-    UnableGetExpenseException
 )
 from app.models import EmailMessage, Expense
 from datetime import datetime, timedelta
 from email.header import decode_header
-from bs4 import BeautifulSoup
-from app.exceptions import UnableGetBodyMessageException
 import email
 import imaplib
 import logging
@@ -65,46 +62,8 @@ class OutlookEmail:
         )
         return subject_decoded, message_content
 
-    def get_clean_html_body(self, raw_message: str) -> str:
-        html_index = raw_message.find('<html')
-        if html_index < 0:
-            raise UnableGetBodyMessageException
-        html_content = raw_message[html_index:]
-        soup = BeautifulSoup(html_content, 'html.parser')
-        body_tag = soup.find('body')
-        if not body_tag:
-            raise UnableGetBodyMessageException
-        return str(body_tag)
-
-    def get_itau_cc_expense(self, message_body: str) -> Expense:
-        """
-        Function to return an expense filtering the info from an Itau
-        credit card expense
-        """
-        soup = BeautifulSoup(message_body, 'html.parser')
-        expense = Expense(0, '')
-        tables = soup.find_all('table')
-        if not tables:
-            raise UnableGetExpenseException
-        try:
-            if len(tables) <= 2:
-                raise UnableGetExpenseException
-            expense.description = tables[0].tr.td.get_text()
-            expense.expense_value = tables[1].find_all('tr')[0] \
-                                             .find_all('td')[1] \
-                                             .get_text()
-            expense.date_expense = tables[1].find_all('tr')[1] \
-                                            .find_all('td')[1] \
-                                            .get_text()
-        except AttributeError:
-            raise UnableGetExpenseException
-        return expense
-
     def get_message_content(self, message_id) -> str:
         ...
 
     def get_email_content(self, email_id: str) -> str:
         return ''
-
-    def get_clean_expense(self, email_content: str) -> Expense:
-        return Expense(datetime.now(), 2, '')
