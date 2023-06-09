a20611d8e404fd547d245a1d4818f701e7c14d06
Jairo Leyton <jali1993@hotmail.com>
2023-06-08 21:43:08 -0500

    Implement function to extract expense values from Bancolombia email

--- app/cleaning_functions.py
+++ app/cleaning_functions.py
@@ -59,3 +59,41 @@ def get_itau_cc_expense(message_body: str) -> Expense:
     except ValueError:
         raise UnableGetExpenseException
     return expense
+
+
+def get_bancolombia_pse_expense(message_body: str) -> Expense | None:
+    """
+    Function to return an expense, cleaning the info from Bancolombia
+    pse payments
+    """
+    soup = BeautifulSoup(message_body, 'html.parser')
+    raw_tx_value = soup.find(string=re.compile('Valor de la'))
+    tx_status = clean_string(str(soup.find(string=re.compile('Estado de'))))
+    tx_value = clean_string(str(raw_tx_value))
+    date_tx = clean_string(str(soup.find(string=re.compile('Fecha de'))))
+    if tx_status.split(':')[1][1:].lower() != 'aprobada':
+        return None
+    try:
+        tx_desc = clean_string(
+            str(raw_tx_value.previous_element.previous_element)
+        )
+    except AttributeError:
+        raise UnableGetExpenseException
+    try:
+        tx_value_clean = tx_value.split(':')[1][1:] \
+                                 .replace('.', '') \
+                                 .replace(',', '.') \
+                                 .replace('$ ', '')
+        return Expense(
+            expense_value=int(tx_value_clean),
+            description=tx_desc,
+            date_expense=datetime.strptime(
+                date_tx.split(':')[1][1:], '%d/%m/%Y'
+            )
+        )
+    except IndexError:
+        raise UnableGetExpenseException
+
+
+def clean_string(str_to_clean: str):
+    return re.sub(r'\s+', ' ', str_to_clean.replace('\n', ' ').strip())
