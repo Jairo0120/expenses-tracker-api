b97b2db760e2f179c90911dfd83ee20b6b4c1d89
Jairo Leyton <jali1993@hotmail.com>
2023-07-24 22:19:11 -0500

    Update main function to filter messages before iterate over them

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import utils
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 from app.outlook_email import OutlookEmail
 from dotenv import load_dotenv
+from cleaning_functions import get_clean_html_body, get_itau_cc_expense
 
 
 def run():
@@ -13,6 +15,15 @@ def run():
     )
     email_service.login()
     emails = email_service.get_unseen_emails(os.getenv('INBOX_NAME', 'inbox'))
+    emails = utils.filter_messages(
+        emails,
+        os.getenv('INCLUDED_SUBJECTS', '').split(',')
+    )
+    for email in emails:
+        body = get_clean_html_body(email.message)
+        print(body)
+        print(get_itau_cc_expense(body))
+    pass
 
 
 if __name__ == '__main__':
