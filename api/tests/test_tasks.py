8aac06e9418edfc8e74cc15edbdddc7625493e58
Jairo Leyton <jali1993@hotmail.com>
2024-06-18 22:39:52 -0500

    Add tests for recurrent incomes generation

--- api/tests/test_tasks.py
+++ api/tests/test_tasks.py
@@ -3,7 +3,7 @@ from sqlmodel.pool import StaticPool
 from datetime import datetime
 from freezegun import freeze_time
 from .. import tasks
-from ..models import User, Cycle
+from ..models import User, Cycle, Income, RecurrentIncome
 import pytest
 
 
@@ -45,7 +45,7 @@ def users_fixture(session: Session):
 
 
 @pytest.fixture(name='active_cycle')
-def cycles_fixture(session: Session, users):
+def active_cycle_fixture(session: Session, users):
     active_cycle = Cycle(
         description=datetime.now().strftime('%B, %Y'),
         start_date=datetime(2024, 1, 1),
@@ -56,11 +56,53 @@ def cycles_fixture(session: Session, users):
     session.commit()
 
 
+@pytest.fixture(name='inactive_cycle')
+def inactive_cycle_fixture(session: Session, users):
+    inactive_cycle = Cycle(
+        description=datetime.now().strftime('%B, %Y'),
+        start_date=datetime(2024, 1, 1),
+        end_date=datetime(2024, 1, 31),
+        user_id=1,
+        is_active=False
+    )
+    session.add(inactive_cycle)
+    session.commit()
+
+
+@pytest.fixture(name='recurrent_incomes')
+def recurrent_incomes_fixture(session: Session, users):
+    recurrent_income_1 = RecurrentIncome(
+        description='Income 1',
+        val_income=1000,
+        user_id=1,
+        created_at=datetime(2024, 1, 1),
+        updated_at=datetime(2024, 1, 1)
+    )
+    recurrent_income_2 = RecurrentIncome(
+        description='Income 2',
+        val_income=40000,
+        user_id=1,
+        created_at=datetime(2024, 1, 1),
+        updated_at=datetime(2024, 1, 1)
+    )
+    session.add(recurrent_income_1)
+    session.add(recurrent_income_2)
+    session.commit()
+
+
 def test_create_cycles_user_active(session: Session, users):
     tasks.create_cycles(session)
     cycles = session.exec(select(Cycle)).all()
     assert len(cycles) == 1
     assert cycles[0].user_id == 1
+
+
+@freeze_time("2024-02-01")
+def test_create_cycle_with_expired_cycle(session: Session, active_cycle):
+    tasks.create_cycles(session)
+    cycles = session.exec(select(Cycle).order_by(Cycle.id.asc())).all()
+    assert len(cycles) == 2
+    assert cycles[0].is_active is False
 
 
 @freeze_time("2024-01-20")
@@ -70,3 +112,23 @@ def test_create_cycles_already_active_cycle(
     tasks.create_cycles(session)
     new_cycles = session.exec(select(Cycle)).all()
     assert len(new_cycles) == 1
+
+
+def test_create_recurrent_incomes_active_cycle(
+        session: Session, active_cycle, recurrent_incomes
+):
+    tasks.create_recurrent_incomes(session)
+    incomes = session.exec(select(Income)).all()
+    cycle = session.exec(select(Cycle)).one()
+    assert len(incomes) == 2
+    assert cycle.is_recurrent_incomes_created == 1
+
+
+def test_create_recurrent_incomes_inactive_cycle(
+        session: Session, inactive_cycle, recurrent_incomes
+):
+    tasks.create_recurrent_incomes(session)
+    incomes = session.exec(select(Income)).all()
+    cycle = session.exec(select(Cycle)).one()
+    assert len(incomes) == 0
+    assert cycle.is_recurrent_incomes_created == 0
