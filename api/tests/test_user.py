5ed5705efa0c863444a9c0a27a6d333cbe5db238
Jairo Leyton <jali1993@hotmail.com>
2024-06-18 21:50:33 -0500

    Update tests for users

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -2,18 +2,33 @@ from fastapi.testclient import TestClient
 from ..models import User
 from ..main import app
 from ..dependencies import get_current_user
+from datetime import datetime
 
 
 client = TestClient(app)
 
 
 async def override_get_current_user_active():
-    return User(id=1, email='test@test.com', name='Test', auth0_id='auth0|1')
+    return User(
+        id=1,
+        email='test@test.com',
+        name='Test',
+        auth0_id='auth0|1',
+        created_at=datetime(2024, 1, 1),
+        updated_at=datetime(2024, 1, 1)
+    )
 
 
 async def override_get_current_user_inactive():
-    return User(id=1, email='test@test.com', name='Test', auth0_id='auth0|1',
-                is_active=False)
+    return User(
+        id=1,
+        email='test@test.com',
+        name='Test',
+        auth0_id='auth0|1',
+        is_active=False,
+        created_at=datetime(2024, 1, 1),
+        updated_at=datetime(2024, 1, 1)
+    )
 
 
 def test_active_user_allow_access():
@@ -32,6 +47,8 @@ def test_active_user_allow_access():
         "is_active": True,
         "start_cycle_day": 1,
         "end_cycle_day": 31,
+        "created_at": "2024-01-01T00:00:00",
+        "updated_at": "2024-01-01T00:00:00"
     }
     app.dependency_overrides = {}
 
