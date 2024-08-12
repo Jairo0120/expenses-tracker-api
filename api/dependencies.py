c504d02f46e9775e7a45b9a3fae164b5a503e7b1
Jairo Leyton <jali1993@hotmail.com>
2024-08-12 10:43:25 -0500

    Replace python-jose with pyjwt to be able to use multiple audiences decoding the token

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,15 +1,16 @@
 from functools import lru_cache
 from pathlib import Path
-from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from api.models import User
 from sqlmodel import Session, create_engine, select
 from . import config
+from cryptography.x509 import load_pem_x509_certificate
+import jwt
 
 
 oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl='token',
+    tokenUrl="token",
 )
 
 
@@ -26,31 +27,37 @@ def get_session():
         yield session
 
 
-async def common_parameters(q: str | None = None, skip: int = 0,
-                            limit: int = 100):
+async def common_parameters(
+    q: str | None = None, skip: int = 0, limit: int = 100
+):
     return {"q": q, "skip": skip, "limit": limit}
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme),
-                           session: Session = Depends(get_session)):
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: Session = Depends(get_session),
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail='Could not validate credentials',
-        headers={'WWW-Authenticate': 'Bearer'}
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
     settings = get_settings()
     try:
-        # python-jose takes care of the validation of the exp date of the token
+        # pyjwt takes care of the validation of the exp date of the token
+        cert_obj = load_pem_x509_certificate(
+            Path(settings.auth0_certificate_url).read_bytes()
+        )
         payload = jwt.decode(
             token,
-            Path(settings.auth0_certificate_url).read_text(),
-            algorithms="RS256",
-            audience=settings.auth0_audience
+            key=cert_obj.public_key(),
+            algorithms=["RS256"],
+            audience=settings.auth0_audience,
         )
-        auth0_id = payload.get('sub')
+        auth0_id = payload.get("sub")
         if auth0_id is None:
             raise credentials_exception
-    except JWTError:
+    except jwt.ExpiredSignatureError:
         raise credentials_exception
     # Check if the user exists
     statement = select(User).where(User.auth0_id == auth0_id)
@@ -62,7 +69,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme),
 
 
 async def get_current_active_user(
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_user),
 ):
     if not current_user.is_active:
         raise HTTPException(status_code=401, detail="Inactive user")
