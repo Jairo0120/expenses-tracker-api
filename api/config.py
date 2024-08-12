be2adee9af38348e7e4c13c8d8da689652ba4d1a
Jairo Leyton <jali1993@hotmail.com>
2024-08-12 10:45:44 -0500

    Update datatype for audience env variable

--- api/config.py
+++ api/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     auth0_certificate_url: str = ""
-    auth0_audience: str = ""
+    auth0_audience: list[str] = []
     sqlite_database_url: str = ""
     log_level: str = "DEBUG"
 
